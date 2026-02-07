3882ae6738342b05eb192354369bf0f75cef095f
Allen Hutchison <allen@hutchison.org>
2026-02-07 09:55:45 -0800

    style: fix prettier formatting

--- workspace-server/src/services/SlidesService.ts
+++ workspace-server/src/services/SlidesService.ts
@@ -210,8 +210,7 @@ export class SlidesService {
         title: presentation.data.title,
         slideCount: presentation.data.slides?.length || 0,
         slides:
-          presentation.data.slides?.map(({ objectId }) => ({ objectId })) ??
-          [],
+          presentation.data.slides?.map(({ objectId }) => ({ objectId })) ?? [],
         pageSize: presentation.data.pageSize,
         hasMasters: !!presentation.data.masters?.length,
         hasLayouts: !!presentation.data.layouts?.length,
